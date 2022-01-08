07ebd12b320a74d8df5a86a40c8e6ae849f0dce4
jinyus <starboymc4@gmail.com>
2022-01-08 05:55:15 -0500

    fix web demo

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5ae03cc31792ac62f0ea684c430e06dc",
-"/": "5ae03cc31792ac62f0ea684c430e06dc",
+"index.html": "2520c5a3093770419579c001484e3349",
+"/": "2520c5a3093770419579c001484e3349",
 "manifest.json": "0867c3e13649ac4d06fe34b7b3ddce08"
 };
 
