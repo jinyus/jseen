a0aba218e7dafbb29308f2d9057690015a8984c6
jinyus <starboymc4@gmail.com>
2022-01-08 04:33:48 -0500

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Jseen Demo'),
+        title: Text('JSeen Demo'),
       ),
       body: Row(
         children: [
